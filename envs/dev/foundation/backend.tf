37d3f3076c3dcb79115a7a4f8f0885fb1eb52669
Matias Da Silva <matu.dasilva@gmail.com>
2026-06-10 09:56:27 -0300

    fix(dev/foundation): align backend key to envs/dev/foundation/terraform.tfstate

--- envs/dev/foundation/backend.tf
+++ envs/dev/foundation/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "tf-platform-foundation-state-dev-938472"
-    key          = "envs/dev/terraform.tfstate"
+    key          = "envs/dev/foundation/terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true
     use_lockfile = true
